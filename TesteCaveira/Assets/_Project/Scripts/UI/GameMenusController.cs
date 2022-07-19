5137f01f6a7a971ab5c3ca0224b4e3376e26a0bb
Anderson Paim <andersonpaim15@outlook.com>
2022-07-19 17:00:21 -0300

    Added quit and restart options in the game over menu

--- TesteCaveira/Assets/_Project/Scripts/UI/GameMenusController.cs
+++ TesteCaveira/Assets/_Project/Scripts/UI/GameMenusController.cs
@@ -1,4 +1,5 @@
 using System;
+using Interfaces;
 using Managers;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,12 +15,18 @@ namespace UI
         [SerializeField] private GameObject _victoryMenu;
         [SerializeField] private GameObject _defeatedMenu;
         [SerializeField] private Button _resumeButton;
+        [SerializeField] private Button _victoryQuitButton;
+        [SerializeField] private Button _victoryRestartButton;
+        [SerializeField] private Button _defeatedQuitButton;
+        [SerializeField] private Button _defeatedRestartButton;
 
+        private ISceneLoader _sceneLoader;
         private bool _isPaused = false;
 
         private void Start()
         {
             StartEvents();
+            Initialize();
         }
 
         private void OnDisable()
@@ -27,11 +34,20 @@ namespace UI
             DestroyEvents();
         }
 
+        private void Initialize()
+        {
+            _sceneLoader = _manager.SceneController.GetComponent<ISceneLoader>();
+        }
+
         private void StartEvents()
         {
             _manager.InputListener.OnPause += PauseGame;
             _manager.OnGameDefeated += ShowDefeatedMenu;
             _resumeButton.onClick.AddListener(ResumeButtonClicked);
+            _victoryQuitButton.onClick.AddListener(QuitButtonClicked);
+            _defeatedQuitButton.onClick.AddListener(QuitButtonClicked);
+            _victoryRestartButton.onClick.AddListener(RestartButtonClicked);
+            _defeatedRestartButton.onClick.AddListener(RestartButtonClicked);
         }
 
         private void DestroyEvents()
@@ -39,6 +55,10 @@ namespace UI
             _manager.InputListener.OnPause -= PauseGame;
             _manager.OnGameDefeated -= ShowDefeatedMenu;
             _resumeButton.onClick.RemoveListener(ResumeButtonClicked);
+            _victoryQuitButton.onClick.RemoveListener(QuitButtonClicked);
+            _defeatedQuitButton.onClick.RemoveListener(QuitButtonClicked);
+            _victoryRestartButton.onClick.RemoveListener(RestartButtonClicked);
+            _defeatedRestartButton.onClick.RemoveListener(RestartButtonClicked);
         }
 
         private void PauseGame()
@@ -60,6 +80,16 @@ namespace UI
             OnPause?.Invoke();
             _isPaused = false;
             _pauseMenu.SetActive(false);
+        }
+
+        private void RestartButtonClicked()
+        {
+            _sceneLoader.RestartScene();
+        }
+
+        private void QuitButtonClicked()
+        {
+            _sceneLoader.LoadScene("MainMenu");
         }
 
         private void ShowDefeatedMenu()
